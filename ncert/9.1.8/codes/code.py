86588bca738489a8d40fb2b825ae3ff7fa65d3b0
ArnavYadnopavit <yadnopavitarnav@gmail.com>
2025-01-06 15:05:55 +0530

    9.1.8

--- ncert/9.1.8/codes/code.py
+++ ncert/9.1.8/codes/code.py
@@ -19,17 +19,21 @@ lib.fx.argtypes = [c_float, c_float]  # float, float
 yn = 0.5  # Initial value for yn
 x = 0.0   # Initial value for x
 
-# Call fx
-results = lib.fx(c_float(yn), c_float(x))
-xvals = np.array([results[i].x for i in range(500)]) #Extracting Values
-yvals = np.array([results[i].y for i in range(500)])
+
 plt.figure(figsize=(8, 6))
-plt.plot(xvals, yvals,linestyle='--',label='sim',color='black')
 
 #Theoritical Plot
-X = np.linspace(0, 2, 500)
+X = np.linspace(0, 2, 2000)
 Y = (np.exp(X)/2)
-plt.plot(X, Y, label="theory", color='yellow')
+plt.plot(X, Y, label="theory", color='red')
+
+# Call fx
+results = lib.fx(c_float(yn), c_float(x))
+xvals = np.array([results[i].x for i in range(2000)]) #Extracting Values
+yvals = np.array([results[i].y for i in range(2000)])
+plt.plot(xvals, yvals,linestyle=':',label='sim',color='black')
+
+
 
 plt.xlabel("x")
 plt.ylabel("y")
--- ncert/9.1.8/codes/func.c
+++ ncert/9.1.8/codes/func.c
@@ -13,13 +13,13 @@ float ffx(float y, float x){
 
 // Function to compute the values of (x, y) using the Euler method
 coords* fx(float yn,float x){
-	float h=0.004;
+	float h=0.001;
 	coords * f;
-	f=(coords*)malloc(500*sizeof(coords));
+	f=(coords*)malloc(2000*sizeof(coords));
 	// Initialize the first coordinates with the given initial values
 	f[0].y=yn;
 	f[0].x=x;
-	for(int i=1;i<500;i++){
+	for(int i=1;i<2000;i++){
 		f[i].y=f[i-1].y+h*ffx(f[i-1].y,f[i-1].x); //y_(n+1)=y_n+h*ffx(y_n,x_n)
 		f[i].x=f[i-1].x+h; //x_(n+1)=x_n+h
 	}
